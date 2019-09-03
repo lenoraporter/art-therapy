e3b3c75d61a93ae5f9fedbed69aa5281ac1b40c2
Lenora Porter <lporter630@gmail.com>
2019-09-03 19:12:07 -0400

    Add three models: Game, picture, and cell. Create a new game each time the controller is initialized.

--- app/controllers/play.js
+++ app/controllers/play.js
@@ -1,5 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    color: 'blue'
+    color: 'blue',
+
+    init() {
+        this._super(...arguments);
+
+        this.set('game', this.store.createRecord('game'));
+    }
 });
